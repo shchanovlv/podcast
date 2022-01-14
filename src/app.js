b32cc1e3dcec3d62ee0cb6ff6c4effa0709d555f
shchanovlv <shchanovlv@mail.ru>
2022-01-14 21:15:59 +0300

    init

---
+++ src/app.js
@@ -0,0 +1,54 @@
+import { Question } from './question.js'
+import { createModal, isValid } from './utils.js'
+import './style.css'
+import { authWithEmailAndPassword, getAuthForm } from './auth.js'
+const form = document.getElementById('form')
+const modalBtn = document.getElementById('modal-btn')
+const input = form.querySelector('#question-input')
+const submitBtn = form.querySelector('#submit')
+
+window.addEventListener('load', Question.renderList)
+form.addEventListener('input', () => {
+  submitBtn.disabled = !isValid(input.value)
+})
+modalBtn.addEventListener('click', openModal)
+form.addEventListener('submit', (event) => {
+  event.preventDefault()
+  if (isValid(input.value)) {
+    const question = {
+      text: input.value.trim(),
+      date: new Date().toJSON(),
+    }
+    submitBtn.disabled = true
+    // async request to server  }
+    Question.create(question).then(() => {
+      input.value = ''
+      input.className = ''
+      submitBtn.disabled = false
+    })
+  }
+})
+function openModal(event) {
+  createModal('Авторизация', getAuthForm())
+  document.getElementById('auth-form').addEventListener('submit', authFormHandler, { once: true })
+}
+function authFormHandler(event) {
+  event.preventDefault()
+  const btn = event.target.querySelector('button')
+  const email = event.target.querySelector('#email').value
+  const password = event.target.querySelector('#password').value
+  btn.disabled = true
+  authWithEmailAndPassword(email, password)
+    .then(Question.fetch)
+    .then(renderModalAfterAuth)
+    .then(() => {
+      btn.disabled = false
+    })
+}
+function renderModalAfterAuth(content) {
+  if (typeof content === 'string') {
+    createModal('Ошибка', content)
+  } else {
+    createModal('Список вопросов', Question.listToHtml(content))
+  }
+}
---
+++ src/question.js
@@ -0,0 +1,70 @@
+export class Question {
+  static create(question) {
+    return fetch(
+      'https://podcast-lschanov-default-rtdb.europe-west1.firebasedatabase.app/questions.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(question),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        question.id = response.name
+        return question
+      })
+      .then(addToLocalStorage)
+      .then(Question.renderList)
+  }
+  static renderList() {
+    const questions = getQuestionToLocalStorage()
+    document.getElementById('list').innerHTML = questions.length
+      ? questions.map(toCard).join('')
+      : `<div class="mui--text-headline">Вы пока ничего не спрашивали</div>`
+  }
+  static fetch(token) {
+    if (!token) {
+      return Promise.resolve('<p class="error">У вас нет токена!</p>')
+    }
+    return fetch(
+      `https://podcast-lschanov-default-rtdb.europe-west1.firebasedatabase.app/questions.json?auth=${token}`
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        if (response && response.error) {
+          return `<p class="error">${response.error}</p>`
+        }
+        return response
+          ? Object.keys(response).map((key) => ({
+              ...response[key],
+              id: key,
+            }))
+          : []
+      })
+  }
+  static listToHtml(questions) {
+    return questions.length
+      ? `<ol>${questions.map((q) => `,<li>${q.text}</li>`).join('')}</ol>`
+      : '<p>Вопросов пока нет</p>'
+  }
+}
+function addToLocalStorage(question) {
+  const all = getQuestionToLocalStorage()
+  all.push(question)
+  localStorage.setItem('questions', JSON.stringify(all))
+}
+function getQuestionToLocalStorage() {
+  return JSON.parse(localStorage.getItem('questions') || '[]')
+}
+function toCard(question) {
+  return `
+    <div class="mui--text-black-54">
+      ${new Date(question.date).toLocaleDateString()}
+      ${new Date(question.date).toLocaleTimeString()}
+    </div>
+    <div>${question.text}</div>
+    <br/>
+  `
+}
